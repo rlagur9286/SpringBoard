4411003d777fed248625e3e8368416ab64093f5b
DESKTOP-PK9B0IA\Hyuk <rlagur9286@naver.com>
2017-12-04 23:33:52 +0900

    2017-12-04, H, init

---
+++ src/main/java/hyuk/sample/dao/SampleDAO.java
@@ -0,0 +1,39 @@
+package hyuk.sample.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import hyuk.common.dao.AbstractDAO;
+
+@Repository("sampleDAO")
+public class SampleDAO extends AbstractDAO {
+
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> selectBoardList(Map<String, Object> map)
+			throws Exception {
+		return (List<Map<String, Object>>) selectList("sample.selectBoardList",	map);
+	}
+
+	public void insertBoard(Map<String, Object> map) {
+		insert("sample.insertBoard", map);
+	}
+	
+	public void updateHitCnt(Map<String, Object> map) throws Exception{
+	    update("sample.updateHitCnt", map);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> selectBoardDetail(Map<String, Object> map) throws Exception{
+	    return (Map<String, Object>) selectOne("sample.selectBoardDetail", map);
+	}
+
+	public void updateBoard(Map<String, Object> map) {
+		update("sample.updateBoard", map);
+	}
+
+	public void deleteBoard(Map<String, Object> map) {
+		update("sample.deleteBoard", map);
+	}
+}
